d325a78b5e4490067f55d1ae33ad068885d91233
somtee001 <ndefosomto2@gmail.com>
2026-06-08 22:25:58 +0100

    made changes to both signin and signup html

--- signup.js
+++ signup.js
@@ -32,3 +32,47 @@ function switchRole(role) {
 
 // default to planner on page load
 switchRole('planner');
+
+// forms
+const nameInput = document.getElementById('plannerFullName');
+const emailInput = document.getElementById('plannerEmail');
+const passwordInput = document.getElementById('plannerPassword');
+const confirmPasswordInput = document.getElementById('plannerConfirmPassword');
+const termsCheckbox = document.getElementById('plannerTerms');
+const submitBtn = document.getElementById('plannerSubmit');
+
+
+function validateForm() {
+    submitBtn.disabled = true;
+
+    if(nameInput.value.trim() !== '' &&
+       emailInput.value.trim() !== '' &&
+       passwordInput.value.trim() !== '' &&
+       confirmPasswordInput.value.trim() !== '' &&
+       passwordInput.value === confirmPasswordInput.value &&
+       termsCheckbox.checked) 
+       {
+        submitBtn.disabled = false;
+        submitBtn.classList.remove('bg-[#a8c4e0]');
+        submitBtn.classList.add('bg-[#0A243F]', 'text-white', 'hover:bg-[#0A243F]/90');
+        submitBtn.style.cursor = 'pointer';
+    } else {
+        submitBtn.disabled = true;
+        submitBtn.classList.remove('bg-[#0A243F]', 'text-white', 'hover:bg-[#0A243F]/90');
+        submitBtn.classList.add('bg-[#a8c4e0]');
+        submitBtn.style.cursor = 'not-allowed';
+    }
+}
+
+nameInput.addEventListener('input', validateForm);
+emailInput.addEventListener('input', validateForm);
+passwordInput.addEventListener('input', validateForm);
+confirmPasswordInput.addEventListener('input', validateForm);
+termsCheckbox.addEventListener('change', validateForm);
+
+plannerForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+    plannerForm.reset();
+    validateForm();
+});
